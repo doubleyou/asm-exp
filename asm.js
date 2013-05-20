4faeeda72dd9982ebb056f11e3f8ffbc8ee4d6e6
Dmitry Demeshchuk <demeshchuk@gmail.com>
2013-05-20 12:05:52 -0700

    Saving

--- asm.js
+++ asm.js
@@ -32,8 +32,26 @@ function Mat4(stdlib, foreign, heap) {
 var arr = new ArrayBuffer(4096);
 var matMod = Mat4(window, {}, arr);
 
-var mat4 = {
-    identity: function() {
-        new Float32Array(arr.slice(16 << 2));
+var dmat4 = {
+    create: function() {
+        new Float32Array(arr.slice(0, 16 << 2));
+    },
+    identity: function(m) {
+        m[0] = 1.0;
+        m[1] = 0.0;
+        m[2] = 0.0;
+        m[3] = 0.0;
+        m[4] = 0.0;
+        m[5] = 1.0;
+        m[6] = 0.0;
+        m[7] = 0.0;
+        m[8] = 0.0;
+        m[9] = 0.0;
+        m[10] = 1.0;
+        m[11] = 0.0;
+        m[12] = 0.0;
+        m[13] = 0.0;
+        m[14] = 0.0;
+        m[15] = 1.0;
     }
 }
