454f4eea2a00e7018a64d8c53d25da7f8249b546
Dmitry Demeshchuk <demeshchuk@gmail.com>
2013-05-20 17:00:54 -0700

    Saving

--- asm.js
+++ asm.js
@@ -33,8 +33,8 @@ var arr = new ArrayBuffer(4096);
 var matMod = Mat4(window, {}, arr);
 
 var dmat4 = {
-    create: function() {
-        var m = new Float32Array(16);
+    identity: function(m) {
+        m = m | new Float32Array(16);
         m[0] = 1.0;
         m[1] = 0.0;
         m[2] = 0.0;
@@ -52,23 +52,7 @@ var dmat4 = {
         m[14] = 0.0;
         m[15] = 1.0;
         return m;
-    },
-    identity: function(m) {
-        m[0] = 1.0;
-        m[1] = 0.0;
-        m[2] = 0.0;
-        m[3] = 0.0;
-        m[4] = 0.0;
-        m[5] = 1.0;
-        m[6] = 0.0;
-        m[7] = 0.0;
-        m[8] = 0.0;
-        m[9] = 0.0;
-        m[10] = 1.0;
-        m[11] = 0.0;
-        m[12] = 0.0;
-        m[13] = 0.0;
-        m[14] = 0.0;
-        m[15] = 1.0;
     }
-}
+};
+
+dmat4.create = dmat4.identity;
