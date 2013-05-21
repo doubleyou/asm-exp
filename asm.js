6fd5b61574d80ab355d62defec6bc78af79f0aa7
Dmitry Demeshchuk <demeshchuk@gmail.com>
2013-05-20 23:43:20 -0700

    Saving

--- asm.js
+++ asm.js
@@ -7,33 +7,36 @@ function Mat4(stdlib, foreign, heap) {
     var OP2_OFFSET = 16;
     var OP3_OFFSET = 32;
     var RESULT_OFFSET = 48;
-    var TABLE_OFFSET = 64;
+    var TABLE_OFFSET = 256;
 
     var H = new stdlib.Float32Array(heap);
+
+    var log = foreign.log;
 
     function identity(n) {
         n = n|0;
 
-        var offset = TABLE_OFFSET|0 + (n|0 << 4);
+        var offset = 0;
+        offset = (TABLE_OFFSET|0 + ((n|0) << 6))|0;
 
-        H[offset] = 1.0;
-        H[offset + 1] = 0.0;
-        H[offset + 2] = 0.0;
-        H[offset + 3] = 0.0;
-        H[offset + 4] = 0.0;
-        H[offset + 5] = 1.0;
-        H[offset + 6] = 0.0;
-        H[offset + 7] = 0.0;
-        H[offset + 8] = 0.0;
-        H[offset + 9] = 0.0;
-        H[offset + 10] = 1.0;
-        H[offset + 11] = 0.0;
-        H[offset + 12] = 0.0;
-        H[offset + 13] = 0.0;
-        H[offset + 14] = 0.0;
-        H[offset + 15] = 1.0;
+        H[((offset|0)) >> 2] = 1.0;
+        H[((offset|0) + 4) >> 2] = 0.0;
+        H[((offset|0) + 8) >> 2] = 0.0;
+        H[((offset|0) + 12) >> 2] = 0.0;
+        H[((offset|0) + 16) >> 2] = 0.0;
+        H[((offset|0) + 20) >> 2] = 1.0;
+        H[((offset|0) + 24) >> 2] = 0.0;
+        H[((offset|0) + 28) >> 2] = 0.0;
+        H[((offset|0) + 32) >> 2] = 0.0;
+        H[((offset|0) + 36) >> 2] = 0.0;
+        H[((offset|0) + 40) >> 2] = 1.0;
+        H[((offset|0) + 44) >> 2] = 0.0;
+        H[((offset|0) + 48) >> 2] = 0.0;
+        H[((offset|0) + 52) >> 2] = 0.0;
+        H[((offset|0) + 56) >> 2] = 0.0;
+        H[((offset|0) + 60) >> 2] = 1.0;
 
-        return offset|0;
+        return ((offset|0) >> 2)|0;
     }
 
     return {
@@ -41,12 +44,12 @@ function Mat4(stdlib, foreign, heap) {
     };
 };
 
-var buffer = new ArrayBuffer(65536);
+var buffer = new ArrayBuffer(4096);
 var array = Float32Array(buffer);
-var mod = Mat4(window, {}, buffer);
+var mod = Mat4(window, {log: function(a) {console.log(a); return 0;}}, buffer);
 
 var dmat4 = {
-    counter: 0;
+    counter: 0
 };
 
 dmat4.identity = function() {
